8bff1c74218b3e9f3392a5cb9f5349d680c10334
Eduard1818 <eduardotumimbo.yns@gmail.com>
2023-05-23 17:20:11 +0800

    CRM-2 Fix layout and UI

---
+++ resources/js/Pages/Layout/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from './Navbar.vue';
+import Sidebar from './Sidebar.vue';
+</script>
+
+<template>
+  <Navbar />
+  <Sidebar />
+
+  <div class="p-4 sm:ml-64">
+    <div class="p-4 mt-14">
+        <slot />
+    </div>
+  </div>
+</template>
